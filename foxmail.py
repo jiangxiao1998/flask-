95971558e3032bde0df8eb58a8f6ef71d20d69a6
jiangxiao1998 <75554118+jiangxiao1998@users.noreply.github.com>
2021-07-04 18:31:28 +0800

    Update foxmail.py

--- foxmail.py
+++ foxmail.py
@@ -3,15 +3,15 @@ from email.mime.text import MIMEText
 
 # 设置服务器所需信息
 # 163邮箱服务器地址
-mail_host = 'smtp.qq.com'
+mail_host = ''
 # 163用户名
-mail_user = '2028835792'
+mail_user = ''
 # 密码(部分邮箱为授权码)
-mail_pass = 'jxgwxcizhsisbajh'
+mail_pass = ''
 # 邮件发送方邮箱地址
-sender = '2028835792@qq.com'
+sender = ''
 # 邮件接受方邮箱地址，注意需要[]包裹，这意味着你可以写多个邮件地址群发
-receivers = ['z1j2t3@foxmail.com']
+receivers = ['']
 
 # 设置email信息
 # 邮件内容设置
